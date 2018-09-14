2d500fc9bb07960a20ddb74405c68e1eb4d2aa2d
Asha George <Asha@Ashas-MBP.home>
2018-09-14 13:54:26 -0400

    added email and password changes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
-export default class LoginForm extends Component {
+class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
   render() {
     return (
       <Card>
@@ -9,22 +19,35 @@ export default class LoginForm extends Component {
           <Input
             label="Email"
             placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
           />
          </CardSection>
 
          <CardSection>
             <Input
-               securityTextEntry
+               secureTextEntry
                label="Password"
                placeholder="password"
+               onChangeText={this.onPasswordChange.bind(this)}
+               value={this.props.password}
             />
         </CardSection>
         <CardSection>
            <Button>
               Login
             </Button>
-        </CardSection>    
+        </CardSection>
       </Card>
     );
   }
 }
+
+const mapStateProps = state => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password
+  };
+};
+
+export default connect(mapStateProps, { emailChanged, passwordChanged })(LoginForm);
