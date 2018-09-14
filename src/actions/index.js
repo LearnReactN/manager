1fce7bbe0d4c067b00ded40ac166cf83a805426d
Asha George <Asha@Ashas-MBP.home>
2018-09-14 13:55:06 -0400

    created action for passwordchanged

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,16 @@
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+
 export const emailChanged = (text) => {
   return {
-    type: 'email_changed',
+    type: EMAIL_CHANGED,
+    payload: text
+  };
+};
+
+//define action create
+export const passwordChanged = (text) => {
+  return {
+    type: PASSWORD_CHANGED,
     payload: text
   };
 };
