a5b4ca056b976bdde317f1e902c372977ed2a8bc
Asha George <Asha@Ashas-MBP.home>
2018-09-14 15:34:54 -0400

    loginuser button press added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
 
 export const emailChanged = (text) => {
@@ -14,3 +15,27 @@ export const passwordChanged = (text) => {
     payload: text
   };
 };
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+   .then(user => {
+     dispatch({ type: 'LOGIN_USER_SUCESS', payload: user });
+   });
+ };
+};
+
+//Default Action Creator Rules
+// .Action creator are functions
+// .Must return an action
+// .An action is an object with a 'type' property
+// and optionally a payload as well
+//{So the whole point of redux-thunk is to allow us
+// to bend these rules that is really what it does.}
+// it allows us to bend the rules if action creators
+
+// Action Creator Rules with Thunk
+// Action creators are functions | or | Action creators are functions
+// Must return an action         | or | Must return a function
+// An action is an object with a | or | This function will be called
+// 'type' property                      with 'dispatch'
